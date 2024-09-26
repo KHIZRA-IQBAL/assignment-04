b7194fa6a9b78f823fcd02ce5cb04d81c1edd125
KHIZRA-IQBAL <iqbalkhizra026@gmail.com>
2024-09-27 01:05:49 +0500

    Your commit message

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,12 +2,10 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const apiUrl = 'https://jsonplaceholder.typicode.com/posts'; 
-  try {
+  
     const res = await fetch(apiUrl);
     const data = await res.json();
 
     return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error:'Failed to fetch data' }, { status: 500 });
-  }
+  
 }
